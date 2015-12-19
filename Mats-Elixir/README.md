617d7e945da160d6d4e690f4d16da628f008e989
Mats Melke <mats@melke.nu>
2015-12-19 23:42:51 +0100

    Day 1 and 2 in Elixir

---
+++ Mats-Elixir/README.md
@@ -0,0 +1,13 @@
+## Advent Of Code 2015, in Elixir
+
+### Run only the advent task tests
+
+```
+mix test --only advent
+```
+
+### Run all tests
+
+```
+mix test
+```
---
+++ Mats-Elixir/lib/day1.ex
@@ -0,0 +1,21 @@
+defmodule Day1 do 
+  require Logger
+
+	def countchars(list, string_to_find) do
+		Enum.filter(list, fn(v) -> v == string_to_find end)
+		|> Enum.count()
+	end
+
+	def find_level_minus_one(list, -1, pos) do
+		{-1,pos}
+	end
+
+	def find_level_minus_one([head|tail], level, pos) do
+		case head do
+			"(" -> find_level_minus_one(tail, level+1, pos+1)
+			")" -> find_level_minus_one(tail, level-1, pos+1)
+			_other -> find_level_minus_one(tail, level, pos+1)
+		end
+	end
+	
+end
