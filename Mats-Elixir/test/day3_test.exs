d593eb23f6810b3c82c3f59453ed9aa51a85b5c0
Mats Melke <mats@melke.nu>
2016-01-07 01:25:14 +0100

    Day 3

--- Mats-Elixir/test/day3_test.exs
+++ Mats-Elixir/test/day3_test.exs
@@ -11,7 +11,7 @@ defmodule Day3Test do
     |> String.codepoints
 
     visited_houses_count = Day3.houses_visited(list)
-		|> Day3.unique_house_visit_count
+    |> Day3.unique_house_visit_count
 
     Logger.debug ("Day 3, Task 1, Houses visited = #{visited_houses_count}")
     assert visited_houses_count == 2565
@@ -25,26 +25,26 @@ defmodule Day3Test do
     |> String.codepoints
 
     visited_houses_count = Day3.houses_visited_together_with_robot(list)
-		|> Day3.unique_house_visit_count
+    |> Day3.unique_house_visit_count
 
     Logger.debug ("Day 3, Task 2, Houses visited together with robot = #{visited_houses_count}")
     assert visited_houses_count == 2639
   end
-	
+  
   test "Visited houses using simple input" do
     assert Day3.houses_visited(String.codepoints("^v^v^v^v^v"))
-		|> Day3.unique_house_visit_count == 2
+    |> Day3.unique_house_visit_count == 2
 
-		assert Day3.houses_visited(String.codepoints("^>v<"))
-		|> Day3.unique_house_visit_count == 4
+    assert Day3.houses_visited(String.codepoints("^>v<"))
+    |> Day3.unique_house_visit_count == 4
   end
   
   test "Visited houses using simple input and robot" do
     assert Day3.houses_visited_together_with_robot(String.codepoints("^v^v^v^v^v"))
-		|> Day3.unique_house_visit_count == 11
+    |> Day3.unique_house_visit_count == 11
 
-		assert Day3.houses_visited_together_with_robot(String.codepoints("^>v<"))
-		|> Day3.unique_house_visit_count == 3
+    assert Day3.houses_visited_together_with_robot(String.codepoints("^>v<"))
+    |> Day3.unique_house_visit_count == 3
   end
   
 end
