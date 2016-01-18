1d4478c407a7b1d13cc713f368ac2634ae24d602
neochrome <johan@stenqvist.net>
2016-01-18 13:04:19 +0100

    added day 5, part 2

--- Johan-OCaml/day5.ml
+++ Johan-OCaml/day5.ml
@@ -23,19 +23,56 @@ let has_forbidden s =
   in
   scan @@ List.of_string s
 
-let is_nice s =
+let has_repeated s =
+  let rec scan = function
+    | [] | [_] | [_;_] -> false
+    | h :: (_ :: (h' :: _) as t) ->
+      if h = h' then true
+      else scan t
+  in
+  scan @@ List.of_string s
+
+let has_pair_twice s =
+  let rec find p = function
+    | [] | [_] -> false
+    | h :: (h' :: _ as t) ->
+      if (h, h') = p then true
+      else find p t
+  in
+  let rec scan = function
+    | x when List.length x < 4 -> false
+    | h :: (h' :: t' as t) ->
+      if find (h, h') t' then true
+      else scan t
+  in
+  scan @@ List.of_string s
+
+let is_nice1 s =
   (has_vowels 3 s) && (has_doubled s) && not (has_forbidden s)
+
+let is_nice2 s =
+  (has_repeated s) && (has_pair_twice s)
 
 
 let part1 =
   File.open_in "day5.input" (fun ch ->
     Stream.of_lines ch
-    |> Stream.filter is_nice
+    |> Stream.filter is_nice1
     |> Stream.to_list
     |> List.length
     |> Printf.printf "part 1: %d nice words found\n"
   )
 
+let part2 =
+  File.open_in "day5.input" (fun ch ->
+    Stream.of_lines ch
+    |> Stream.filter is_nice2
+    |> Stream.to_list
+    |> List.length
+    |> Printf.printf "part 2: %d nice words found\n"
+  )
+
 
 let () =
   part1;
+  part2;
