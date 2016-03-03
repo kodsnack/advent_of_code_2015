168f3781861564358f0925632f75d2ca9b4bdd31
Christer Lennell <karlschrister@hotmail.com>
2016-03-03 08:37:25 +0100

    Day 2 of advent of code

---
+++ Christer-java/README.md
@@ -0,0 +1,2 @@
+#Day1 completed
+java -cp com.lennell.Day1 input.txt
--- Christer-java/src/com/lennell/Day1.java
+++ Christer-java/src/com/lennell/Day1.java
@@ -1,19 +1,25 @@
 package com.lennell;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Day1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         if (args.length!=1){
-            System.out.println("Program takes one String parameter!");
+            System.out.println("Program takes one String parameter. Filename to input file!");
             return;
         }
 
+        String fileName = args[0];
+
+        List<String> lines = Files.readAllLines(Paths.get(fileName));
+
         List<Character> charList = new ArrayList<>();
-        for (char c:args[0].toCharArray()){
+        for (char c:lines.get(0).toCharArray()){
             charList.add(c);
         }
         long left = charList.stream().filter(p->p.equals('(')).count();
