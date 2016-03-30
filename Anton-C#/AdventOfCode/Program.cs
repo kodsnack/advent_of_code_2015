bf32821172c4d5f8c81640508f06e60156b72f44
Anton <olason@posteo.se>
2016-03-30 17:41:55 +0200

    Dag 14

--- Anton-C#/AdventOfCode/Program.cs
+++ Anton-C#/AdventOfCode/Program.cs
@@ -19,7 +19,8 @@ namespace Adventofcode
             //Day10();
             //Day11.Run();
             //Day12.Run();
-            Day13.Run();
+            //Day13.Run();
+            Day14.Run();
             Console.WriteLine("Färdig!");
             Console.ReadKey();
 
